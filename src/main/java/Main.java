7f94a68a3f80963c0a72d1b9cb1fd39391b245ad
AndresSolvd <agarcianavarro.laba@solvd.com>
2023-05-13 10:58:50 -0400

    12 from 31 doing "Java program to display all the multiples of 3 within the range 10 to 50"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.Math.sqrt;
 
 public class Main {
@@ -42,6 +45,83 @@ public class Main {
         return total;
     }
 
+    //Java program to find the circumference and area of a circle with a given radius
+    public static Map<String, Double> getCircumferenceArea(double radius) {
+        double area = Math.pow(radius, 2) * Math.PI;
+        double circumference = radius * 2 * Math.PI;
+        Map<String, Double> result = new HashMap<>();
+        result.put("Circumference", circumference);
+        result.put("Area", area);
+        return result;
+    }
+
+    //Java program to check whether the given integer is a multiple of 5
+    public static String isMultipleOfFive(int number) {
+        if (number % 5 == 0) {
+            return "Yes it is multiple of 5";
+        } else {
+            return "No it is not a multiple of 5";
+        }
+    }
+
+    //Java program to check whether the given integer is a multiple of both 5 and 7
+    public static String isMultipleOfFiveAndSeven(int number) {
+        if (number % 35 == 0) {
+            return "Yes it is a multiple of 5 and 7";
+        } else {
+            return "No it is not a multiple of 5 and 7";
+        }
+    }
+
+    //Java program to find the average of 5 numbers using a while loop
+    public static double averageFiveNumbers(double[] numbers) {
+        int i = 0;
+        double sum = 0;
+        while(i != numbers.length){
+            sum = sum + numbers[i];
+            i++;
+        }
+        return sum / numbers.length;
+    }
+
+    //Java program to display the given integer in the reverse order
+    public static int reverseInteger(int number) {
+        /*for (int i = number.length() - 1; i >= 0; i--) {
+            System.out.print(number.charAt(i));
+        }*/
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return number;
+    }
+
+    //Java program to find the geometric mean of n numbers
+    public static double getArithmeticMean(double[] numbers) {
+        double product = 1;
+        for (double number : numbers) {
+            product = product * number;
+        }
+        return Math.pow(product,  1.0 / numbers.length);
+    }
+
+    //Java program to find the sum of the digits of an integer using a while loop
+    public static int digitsSum(int number) {
+        int sum = 0;
+        while (number != 0 ) {
+            sum = sum  + number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+
+    //Java program to display all the multiples of 3 within the range 10 to 50
+
+
+
 
     public static void main(String[] args) {
 
@@ -49,25 +129,26 @@ public class Main {
         System.out.println(calculateFahrenheitFromCelsius(30) + " Fahrenheit");
         System.out.println("The area is: " + calculateAreaTriangle(4, 3, 4));
         System.out.println("The average is: " + average(new int[]{2, 3, 4, 5, 1, 3, 10}));
-        System.out.println("the product of this set of Real Numbers is: " + calculateRealNumberProduct(new double[]{Math.PI, 23, 44, Math.E, 34.23}));
+        System.out.println("The product of this set of Real Numbers is: " +
+                calculateRealNumberProduct(new double[]{Math.PI, 23, 44, Math.E, 34.23}));
+        Map<String, Double> results = getCircumferenceArea(5);
+        System.out.println("The circumference is: " + results.get("Circumference")
+                + ". The area is: " + results.get("Area"));
+        System.out.println(isMultipleOfFive(1235));
+        System.out.println(isMultipleOfFiveAndSeven(35));
+        System.out.println(averageFiveNumbers(new double[]{1, 2, 3, 4, 5}));
+        System.out.println("The reverse is: " + reverseInteger(987654321));
+        System.out.println("The arithmetic mean is: " + getArithmeticMean(new double[]{2,8}));
+        System.out.println("The sum of the digits of the integer is: " + digitsSum(558));
 
     }
 }
 
 
 /*•
+
 •
 •
-•
-•
-• Java program to find the circumference and area of a circle with a given radius
-• Java program to check whether the given integer is a multiple of 5
-• Java program to check whether the given integer is a multiple of both 5 and 7
-• Java program to find the average of 5 numbers using a while loop
-• Java program to display the given integer in the reverse order
-• Java program to find the geometric mean of n numbers
-• Java program to find the sum of the digits of an integer using a while loop
-• Java program to display all the multiples of 3 within the range 10 to 50
 • Java program to display all integers within the range 100-150 whose sum of digits is an even number
 • Java program to check whether the given integer is a prime number or not
 • Java program to generate the prime numbers from 1 to N

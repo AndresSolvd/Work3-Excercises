3b869604ad326cc6ac00de7b8c170806b2aa4008
andre <abinary1010@gmail.com>
2025-02-28 10:04:13 -0500

    separate the titles and create a new try

---
+++ src/main/java/SecondTry.java
@@ -0,0 +1,53 @@
+
+public class SecondTry {
+
+    //1 Java program to check whether the given number is even or odd
+    //2 Java program to convert the temperature in Celsius to Fahrenheit
+    //3 Java program to find the area of a triangle whose three sides are given
+    //4 Java program to find out the average of a set of integers
+    //5 Java program to find the product of a set of real numbers
+    //6 Java program to find the circumference and area of a circle with a given radius
+    //7 Java program to check whether the given integer is a multiple of 5
+    //8 Java program to check whether the given integer is a multiple of both 5 and 7
+    //9 Java program to find the average of 5 numbers using a while loop
+    //10 Java program to display the given integer in the reverse order
+    //11 Java program to find the geometric mean of n numbers
+    //12 Java program to find the sum of the digits of an integer using a while loop
+    //13 Java program to display all the multiples of 3 within the range 10 to 50
+    //14 Java program to display all integers within the range 100-150 whose sum of digits is an even number
+    //15 Java program to check whether the given integer is a prime number or not
+    //16 Java program to generate the prime numbers from 1 to N
+    //17 Java program to find the roots of a quadratic equation
+    //18 Java program to print the numbers from a given number n till 0 using recursion
+    //19 Java program to find the factorial of a number using recursion
+    //20 Java program to display the sum of n numbers using an array
+    //21 Java program to implement linear search
+    //22 Java program to find the number of odd numbers in an array
+    //23 Java program to find the largest number in an array without using built-in functions
+    //24 Java program to insert a number to any position in an array
+    //25 Java program to delete an element from an array by index
+    //26 Java program to check whether a string is a palindrome or not
+    //27 Java program to implement matrix addition
+    //28 Java program to implement matrix multiplication
+    //29 Java program to check leap year
+    //30 Java program to find the Nth term in a Fibonacci series using recursion
+    //31 Java program to print Fibonacci series using iteration
+    //32 Java program to implement a calculator to do basic operations
+    //33 Java Program to Find Your Weight on Mars
+    //34 Java Program to Check Whether the Generated Random Number Is Even or Odd
+    /*35 Java Program to Find the Number of Containers You Need. Given a number
+    N and a time X unit, the task is to find the number of containers that are
+    filled completely in X unit if containers are arranged in pyramid fashion as shown below.
+    */
+    //36 Java Program to Calculate Taxes
+    //37 Calculate BMI Using Java
+    //38 Java Program to Find the Sum of Even Numbers in a range
+    //39 Java Program to Find the Largest and Smallest Numbers From Random Numbers
+    //40 Java Program to Calculate the Area of a Rectangle
+    //41 Filling an Array Using For Loops in Java (enter type (1 dimension array or 2D array) and size from the keyboard too)
+    //42 Java Program to Find the Largest and Smallest Numbers From an Array of Random Numbers
+/*•
+https://code-exercises.com/programming/medium/
+*/
+}
+
